5377042c6156d568b886a090710de685b0bbce3d
michael <michael.cashmore@strath.ac.uk>
2019-11-05 12:23:43 +0000

    updated sensing interface to automatically update KB with waypoints and objects

--- rosplan_demo_stage/scripts/sampling_sensing_functions.py
+++ rosplan_demo_stage/scripts/sampling_sensing_functions.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import numpy as np
+from rosplan_knowledge_msgs.srv import KnowledgeUpdateServiceArray, KnowledgeUpdateServiceArrayRequest
+from rosplan_knowledge_msgs.msg import KnowledgeItem
+from rosplan_knowledge_msgs.srv import GetInstanceService
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
-from rosplan_knowledge_msgs.srv import GetInstanceService
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 from math import sqrt
@@ -47,15 +50,63 @@ def robot_at(msg, params): # Idea: Params are the instances of all the parameter
 
 def pedestal_visible_from(res, params):
 
+    rospy.wait_for_service('/rosplan_knowledge_base/update_array')
+    update_kb = rospy.ServiceProxy('/rosplan_knowledge_base/update_array', KnowledgeUpdateServiceArray)
+
     assert(len(params) == 2)
     ret_value = []
-    attributes = get_kb_attribute("pedestal_visible_from")
+    skip_check = False
 
+    # load and check KB for objects
     doughnuts = []
     if rospy.has_param('~doughnuts'):
         doughnuts = rospy.get_param('~doughnuts')
+    if len(params[1]) != len(doughnuts):
+        del params[1][:]
+        # objects not properly loaded yet, add to KB
+        rospy.loginfo("KCL: (%s) Objects not loaded into KB yet, skipping visibility check" % rospy.get_name())
+        try:
+            kus = KnowledgeUpdateServiceArrayRequest()
+            for d in doughnuts:
+                ki = KnowledgeItem()
+                ki.instance_type = d['type']
+                ki.instance_name = d['name']
+                ki.knowledge_type = ki.INSTANCE
+                kus.update_type += np.array(kus.ADD_KNOWLEDGE).tostring()
+                kus.knowledge.append(ki)
+                params[1].append(d['name'])
+            res = update_kb(kus)
+        except rospy.ServiceException, e:
+            rospy.logerr("KCL (%s) Failed to update knowledge base: %s" % rospy.get_name(), e.message)
+        skip_check = True
 
-    assert(len(params[1])==len(doughnuts))
+    # load and check KB for waypoints
+    waypoints = []
+    if rospy.has_param("/task_planning_waypoints"):
+        waypoints = rospy.get_param('/task_planning_waypoints')
+    if len(params[0]) != len(waypoints):
+        del params[0][:]
+        # waypoints not properly loaded yet, add to KB
+        rospy.loginfo("KCL: (%s) Waypoints not loaded into KB yet, skipping visibility check" % rospy.get_name())
+        try:
+            kus = KnowledgeUpdateServiceArrayRequest()
+            for wp in waypoints:
+                ki = KnowledgeItem()
+                ki.instance_type = "waypoint"
+                ki.instance_name = wp
+                ki.knowledge_type = ki.INSTANCE
+                kus.update_type += np.array(kus.ADD_KNOWLEDGE).tostring()
+                kus.knowledge.append(ki)
+                params[0].append(wp)
+            res = update_kb(kus)
+        except rospy.ServiceException, e:
+            rospy.logerr("KCL (%s) Failed to update knowledge base: %s" % rospy.get_name(), e.message)
+        skip_check = True
+
+    if skip_check:
+        return ret_value
+
+    attributes = get_kb_attribute("pedestal_visible_from")
 
     topic = 'pedestal_visible_from'
     publisher = rospy.Publisher(topic, MarkerArray, queue_size=1)
