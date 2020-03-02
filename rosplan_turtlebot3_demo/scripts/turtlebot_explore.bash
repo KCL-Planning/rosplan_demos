ae9e39846c2a684a6a122aef9c3eff042b02aa8c
Gerard Canal <grar.knal@gmail.com>
2020-03-02 09:16:43 +0000

    Fix namespaces for tb3 demo as in #25

--- rosplan_turtlebot3_demo/scripts/turtlebot_explore.bash
+++ rosplan_turtlebot3_demo/scripts/turtlebot_explore.bash
@@ -2,7 +2,7 @@
 
 # call rosplan mapping interface to generate waypoints based on costmap
 echo "Generating waypoints.";
-rosservice call /kcl_rosplan/rosplan_roadmap_server/create_prm "{nr_waypoints: 10, min_distance: 0.3, casting_distance: 2.0, connecting_distance: 8.0, occupancy_threshold: 10, total_attempts: 10}";
+rosservice call /rosplan_roadmap_server/create_prm "{nr_waypoints: 10, min_distance: 0.3, casting_distance: 2.0, connecting_distance: 8.0, occupancy_threshold: 10, total_attempts: 10}";
 
 # call turtlebot_explore_common.bash script
 bash `rospack find rosplan_turtlebot2_demo`/scripts/turtlebot_explore_common.bash
